f8af8a3be0b2db739975a08308d1685efa6b19bf
mpksimar <53793545+mpksimar@users.noreply.github.com>
2019-08-13 18:56:09 -0400

    Update query.js

--- query.js
+++ query.js
@@ -5,20 +5,20 @@ jQuery(document).ready(function(){ //is needed to perform this function only aft
 });
 
 //2.Apply css border of (1px solid green) elements containing attribute name ONLY ‘marlabs’
-//jQuery("p[name|= 'marlabs']").css('border', '1px solid green');
+jQuery("p[name|= 'marlabs']").css('border', '1px solid green');
 
 //3.Apply css border of (1px solid green) to all elements starting with attribute name ‘marlabs’
-//jQuery("p[name^= 'marlabs']").css('border', '1px solid green');
+jQuery("p[name^= 'marlabs']").css('border', '1px solid green');
 
 //4.Apply css border of (1px solid green) to all elements ending with attribute name ‘marlabs’
-//jQuery("p[name$= 'marlabs']").css('border', '1px solid green');
+jQuery("p[name$= 'marlabs']").css('border', '1px solid green');
 
 
 //5.Apply css border of (1px solid green) to all elements containing WORD ‘marlabs’
-//jQuery("p[name~= 'marlabs']").css('border', '1px solid green');
+jQuery("p[name~= 'marlabs']").css('border', '1px solid green');
 
 //6.Apply css border of (1px solid green) to the last element in div
-//jQuery("div p:last-child").css('border', '1px solid green');
+jQuery("div p:last-child").css('border', '1px solid green');
 
 // Give one examples of each that prints ‘Hello marlabs’
 
@@ -34,28 +34,28 @@ jQuery(document).ready(function(){ //is needed to perform this function only aft
 // .dblclick()
 
 // 1 .on()
-// jQuery(document).ready(function(){
-// $("#target").on("click",function(){
-//   console.log($(this).text());
-// });
-//
-// });
+jQuery(document).ready(function(){
+$("#target").on("click",function(){
+  console.log($(this).text());
+});
+
+});
 
 //2 .one()-the event will occur only once
-// jQuery(document).ready(function(){
-// $("#target").one("click",function(){
-//   console.log($(this).text());
-// });
-//
-// });
+jQuery(document).ready(function(){
+ $("#target").one("click",function(){
+   console.log($(this).text());
+ });
+
+});
 
 //3 .trigger()
-// jQuery(document).ready(function(){
-// $("#target").on("click",function(){
-//   alert($(this).text());
-// });
-// $("#target").trigger("click");
-// });
+ jQuery(document).ready(function(){
+ $("#target").on("click",function(){
+   alert($(this).text());
+ });
+ $("#target").trigger("click");
+});
 
 //4 .scroll()
 jQuery(document).ready(function(){
@@ -105,12 +105,12 @@ $( document ).keyup(function() {
 });
 
 //10 .dbclick()
-//jQuery(document).ready(function(){
+jQuery(document).ready(function(){
 
-//$("button" ).click(function() {
-  //console.log("double-clicked");
-//});
-//});
+$("button" ).click(function() {
+  console.log("double-clicked");
+});
+});
 
 $(document).ready(function() {
 $("p").dblclick(function(){
@@ -119,15 +119,6 @@ $(this).text("Hello Marlabs");
     });
   });
 
-//get call
-var request= newXMLHttpRequest();
-request.open('GET', "https://pokeapi.co/api/v2/pokemon/ditto", true)
 
-request.onload= function(){
-  var data= JSON.parse(this.response);
-  if(request.status>=200 && request.status= 400)
 
-}
-}
 
-request.send();
